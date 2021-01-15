d13a5ef92ec785715d8b3ca51727f80a2a400086
Jared Long <jlong4223@gmail.com>
2021-01-15 14:38:51 -0600

    adds notfound component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <Navbar />
+  <div id="notfound">
+    <h1>Oops...404 Not Found</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+
+export default {
+  name: "NotFound",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style scoped>
+#notfound {
+  display: flex;
+  justify-content: center;
+}
+</style>
