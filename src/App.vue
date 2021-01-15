7307e9ff58fb03654e4b641fca99e6a03b23602c
Jared Long <jlong4223@gmail.com>
2021-01-15 10:41:41 -0600

    fetchs data from routes on backend API

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,37 @@
 </template>
 
 <script>
+// imported components
 import Products from "./components/Products";
+
+// defined variables for fetch
+const BASE_URL = "http://localhost:3001/";
 
 export default {
   name: "App",
   components: {
     Products,
   },
+  data: () => ({
+    error: "",
+    products: [],
+    services: [],
+  }),
+
+  mounted() {
+    fetch(BASE_URL + "products")
+      .then((res) => res.json())
+      .then((results) => {
+        this.products = results;
+      }),
+      fetch(BASE_URL + "services")
+        .then((res) => res.json())
+        .then((results) => {
+          this.services = results;
+        });
+  },
+  // methods are where CRUD routes go
+  methods: {},
 };
 </script>
 
