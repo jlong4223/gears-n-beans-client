d7ad3a3c9e7eae90d9e777d8aee61e23d30c0daa
Jared Long <jlong4223@gmail.com>
2021-01-15 12:27:28 -0600

    adds components for later routes

---
+++ src/components/Services.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><h1>Hi, welcome to the services page</h1></div>
+</template>
+
+<script>
+export default {
+  name: "Services",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
