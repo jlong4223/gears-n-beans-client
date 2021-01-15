c8a589ad2c2492a98de6b2c4396fc6034f84b276
Jared Long <jlong4223@gmail.com>
2021-01-15 14:38:35 -0600

    adds 404 route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "@/components/Home.vue";
 import Services from "@/components/Services.vue";
 import Products from "@/components/Products.vue";
+import NotFound from "@/components/NotFound.vue";
+
 const routes = [
   {
     path: "/",
@@ -17,6 +19,10 @@ const routes = [
     path: "/products",
     name: "Products",
     component: Products,
+  },
+  {
+    path: "/:catchAll(.*)",
+    component: NotFound,
   },
 ];
 
