422e582d4365e6c1b1e40a892fcec60206eaeb41
Jared Long <jlong4223@gmail.com>
2021-01-18 10:15:03 -0600

    adds height to notfound div

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -3,15 +3,18 @@
   <div id="notfound">
     <h1>Oops...404 Not Found</h1>
   </div>
+  <Footer />
 </template>
 
 <script>
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default {
   name: "NotFound",
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
@@ -20,5 +23,6 @@ export default {
 #notfound {
   display: flex;
   justify-content: center;
+  height: 400px;
 }
 </style>
