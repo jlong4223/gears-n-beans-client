14c047527e862a9223de7b7ce1bcff673cf46798
Jared Long <jlong4223@gmail.com>
2021-02-06 16:58:56 -0600

    sets up commercejs in mainjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
+// import Vue from "vue";
+// import commercejs from "./commerce/configCommercejs";
+
+// Vue.config.productionTip = false;
+
+// Vue.mixin({
+//   beforeCreate() {
+//     this.$commerce = commercejs;
+//   },
+// });
 
 createApp(App)
   .use(router)
