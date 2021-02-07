14bb9bd0d74d4115e4d86f5018d18a75e98f5ced
Jared Long <jlong4223@gmail.com>
2021-02-07 10:19:11 -0600

    adds navbar and footer to reviews page

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,10 +1,19 @@
 <template>
+  <Navbar />
   <div>reviews page</div>
+  <Footer />
 </template>
 
 <script>
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
 export default {
   name: "Reviews",
+  components: {
+    Navbar,
+    Footer,
+  },
 };
 </script>
 
