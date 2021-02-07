ea8c7354d0d000e0c06fcdbbb128fd98f3d49324
Jared Long <jlong4223@gmail.com>
2021-02-07 10:24:57 -0600

    grabs the review data from backend

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -7,12 +7,23 @@
 <script>
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
+const BASE_URL = "https://gearsbeans-api.herokuapp.com/";
 export default {
   name: "Reviews",
   components: {
     Navbar,
     Footer,
+  },
+  data: () => ({
+    error: "",
+    reviews: [],
+  }),
+  mounted() {
+    fetch(BASE_URL + "reviews")
+      .then((res) => res.json())
+      .then((results) => {
+        this.reviews = results;
+      });
   },
 };
 </script>
