1f8765b1658d9292344c18aaa2e35820bdc45847
Jared Long <jlong4223@gmail.com>
2021-02-07 10:31:25 -0600

    showing review info

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,6 +1,18 @@
 <template>
   <Navbar />
-  <div>reviews page</div>
+  <div>
+    <div class="card_container">
+      <h3>
+        {{ about }}
+      </h3>
+    </div>
+    <div v-for="review in reviews" v-bind:key="review.id">
+      <h4>{{ review.name }}</h4>
+      <h4>{{ review.message }}</h4>
+      <h4>{{ review.product }}</h4>
+      <h4>{{ review.stars }}</h4>
+    </div>
+  </div>
   <Footer />
 </template>
 
@@ -17,6 +29,7 @@ export default {
   data: () => ({
     error: "",
     reviews: [],
+    about: "Check out our reviews",
   }),
   mounted() {
     fetch(BASE_URL + "reviews")
@@ -28,4 +41,23 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.card_container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  height: 500px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("https://wpcdn.us-east-1.vip.tn-cloud.net/www.klkntv.com/content/uploads/2020/05/empty-bikes.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+h3 {
+  text-align: center;
+  width: 700px;
+  background-color: transparent;
+  color: white;
+  font-size: 45px;
+}
+</style>
