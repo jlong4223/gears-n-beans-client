a8ffbcf34ac9de3125cf6a39e54467918217c1eb
Jared Long <jlong4223@gmail.com>
2021-03-03 23:07:55 -0600

    sets up google map with vue3 npm package - successfully

--- src/components/GMapCopy.vue
+++ src/components/GMapCopy.vue
@@ -1,44 +1,24 @@
 <template>
-  <div>
-    <div class="map"></div>
-  </div>
+  <GoogleMap
+    :api-key="API_KEY"
+    style="width: 100%; height: 300px"
+    :center="center"
+    :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
 </template>
 
 <script>
-// import { ref, onMounted } from "vue";
-export default {
-  name: "GMapCopy",
-  data() {
-    return {
-      map: null,
-      mapCenter: { lat: 0, lng: 0 },
-    };
-  },
-  mounted() {
-    this.initMap();
-  },
-  methods: {
-    initMap() {
-      this.map = new window.google.maps.Map(document.getElementById("map"), {
-        center: this.mapCenter,
-        zoom: true,
-        maxZoom: 20,
-        minZoom: 3,
-        streetViewControl: true,
-        mapTypeControl: true,
-        fullScreenControl: true,
-        zoomControl: true,
-      });
-    },
-  },
-};
-</script>
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
 
-<style scoped>
-.map {
-  width: 100%;
-  height: 300px;
-  background-color: azure;
-  border: 3px solid green;
-}
-</style>
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 40.689247, lng: -74.044502 };
+    const API_KEY = "AIzaSyAX2UsD6kMq5dalMOx5_o0cJe_GHRLd_Nw";
+    return { center, API_KEY };
+  },
+});
+</script>
