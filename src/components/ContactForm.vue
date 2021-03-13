14d96c3984846a707e756f325fbcb0a39cf7f207
Jared Long <jlong4223@gmail.com>
2021-03-12 21:26:00 -0600

    sets up contact form component

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="contactPage">
+    <Navbar />
+    <main id="form">
+      <h1>This is the contact form page</h1>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default {
+  name: "ContactForm",
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
+
+<style scoped>
+#contactPage {
+  display: flex;
+  flex-direction: column;
+}
+#form {
+  height: 500px;
+}
+</style>
